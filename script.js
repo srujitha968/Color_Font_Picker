1e5d8c0b6c047c86d56fe66c9c50c1c21adc456b
srujitha968 <vasaviamara2002@gmail.com>
2024-11-26 23:12:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,264 @@
+function redtextcol() {
+    let t=document.getElementById("text")
+    t.style.color="red"
+}
+function greentextcol() {
+    let t=document.getElementById("text")
+    t.style.color="green"
+}
+function pinktextcol() {
+    let t=document.getElementById("text")
+    t.style.color="pink"
+}
+function yellowtextcol() {
+    let t=document.getElementById("text")
+    t.style.color="yellow"
+}
+function bluetextcol() {
+    let t=document.getElementById("text")
+    t.style.color="blue"
+}
+function violettextcol() {
+    let t=document.getElementById("text")
+    t.style.color="violet"
+}
+function orangetextcol() {
+    let t=document.getElementById("text")
+    t.style.color="orange"
+}
+function hotpinktextcol() {
+    let t=document.getElementById("text")
+    t.style.color="hotpink"
+}
+function lightbluetextcol() {
+    let t=document.getElementById("text")
+    t.style.color="lightblue"
+}
+function greytextcol() {
+    let t=document.getElementById("text")
+    t.style.color="grey"
+}
+function lightgreentextcol() {
+    let t=document.getElementById("text")
+    t.style.color="lightgreen"
+}
+function purpletextcol() {
+    let t=document.getElementById("text")
+    t.style.color="purple"
+}
+function browntextcol() {
+    let t=document.getElementById("text")
+    t.style.color="brown"
+}
+function whitetextcol() {
+    let t=document.getElementById("text")
+    t.style.color="white"
+}
+function tealtextcol() {
+    let t=document.getElementById("text")
+    t.style.color="teal"
+}
+function maroontextcol() {
+    let t=document.getElementById("text")
+    t.style.color="maroon"
+}
+function auqatextcol() {
+    let t=document.getElementById("text")
+    t.style.color="aqua"
+}
+function minttextcol() {
+    let t=document.getElementById("text")
+    t.style.color="rgb(103,245,173)"
+}
+function peachtextcol() {
+    let t=document.getElementById("text")
+    t.style.color="peachpuff"
+}
+function olivetextcol() {
+    let t=document.getElementById("text")
+    t.style.color="olive"
+}
+function lightredtextcol() {
+    let t=document.getElementById("text")
+    t.style.color="orangered"
+}
+function darkredtextcol() {
+    let t=document.getElementById("text")
+    t.style.color="darkred"
+}
+function perutextcol() {
+    let t=document.getElementById("text")
+    t.style.color="peru"
+}
+function greenbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="green"
+}
+function redbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="red"
+}
+function pinkbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="pink"
+}
+function yellowbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="yellow"
+}
+function bluebgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="blue"
+}
+function violetbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="violet"
+}
+function orangebgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="orange"
+}
+function hotpinkbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="hotpink"
+}
+function lightbluebgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="lightblue"
+}
+function greybgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="grey"
+}
+function lightgreenbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="lightgreen"
+}
+function purplebgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="purple"
+}
+function brownbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="brown"
+}
+function tealbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="teal"
+}
+function maroonbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="maroon"
+}
+function auqabgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="aqua"
+}
+function mintbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="rgb(103,245,174)"
+}
+function peachbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="peachpuff"
+}
+function olivebgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="olive"
+}
+function orangeredbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="orangered"
+}
+function darkredbgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="darkred"
+}
+function perubgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="peru"
+}
+function whitebgcolo() {
+    let t=document.getElementById("text")
+    t.style.backgroundColor="white"
+}
+function a() {
+    let t=document.getElementById("text")
+    t.style.fontStyle="italic"
+}
+function b() {
+    let t=document.getElementById("text")
+    t.style.fontFamily = "Georgia"
+}
+function c() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Verdana"
+}
+function d() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Tahoma"
+}
+function e() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Courier New"
+}
+function f() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Georgia"
+}
+function g() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="'Comic Sans MS','cursive'"
+}
+function h() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Impact"
+}
+function i() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="'Palatino Linotype','Book Antiqua'"
+}
+function j() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="'Lucida Console','monospace'"
+}
+function k() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Helvetica"
+}
+function l() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Trebuchet"
+}
+function m() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="'Brush Script Mt',cursive"
+}
+function n() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Lucida Sans"
+}
+function o() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Gill Sans"
+}
+function p() {
+    let t=document.getElementById("text")
+    t.style.fontStyle="Franklin Gothic Medium"
+}
+function q() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Garamond"
+}
+function r() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Segoe UI"
+}
+function s() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Rockwell"
+}
+function t() {
+    let t=document.getElementById("text")
+    t.style.fontFamily="Century Gothic"
+}
